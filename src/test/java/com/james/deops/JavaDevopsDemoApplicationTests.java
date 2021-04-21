ec34ef834c2ad7042b2a38ec0cc292e0569bbde6
JamesZhao007 <zhepengzhao@gmail.com>
2021-04-21 13:05:15 -0400

    init

---
+++ src/test/java/com/james/deops/JavaDevopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.james.deops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevopsDemoApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
